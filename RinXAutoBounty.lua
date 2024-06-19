2b3708c8e77e0fa19c04b894d0cca341274bc22f
RinXHub <167017943+RinXHub@users.noreply.github.com>
2024-06-19 17:25:36 +0700

    Delete RinXAutoBounty.lua

--- RinXAutoBounty.lua
+++
@@ -1,7 +0,0 @@
-setclipboard("https://discord.gg/tdDuFmgd4r")
-local msg = [==[
-Join Discord For The New Update Script |
-Vào Discord Để Lấy Script mới nhất
- | https://discord.gg/tdDuFmgd4r
-]==]
-game:GetService("Players").LocalPlayer:Kick(msg)
